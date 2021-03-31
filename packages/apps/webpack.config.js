641d4daa721a09d902f83a0b69e9265ce75dd00b
吴wbh1328551759 <1328551759@qq.com>
2021-03-31 17:56:05 +0800

    fix polkadot balance

--- packages/apps/webpack.config.js
+++ packages/apps/webpack.config.js
@@ -29,7 +29,7 @@ module.exports = merge(
       }),
       new AliosscdnWebpackPlugin({
         https: true,
-        directoryInOss: 'platdot/v1.0.0',
+        directoryInOss: 'platdot/v1.0.1',
         filesPath: `${__dirname}/build`,
         region: CLIENT.region,
         accessKeyId: CLIENT.accessKeyId,
