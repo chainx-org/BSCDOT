2b983da48f0943457a22fecab475e5a25f543134
吴wbh1328551759 <1328551759@qq.com>
2021-04-15 10:46:48 +0800

    update webpack.config.js

--- packages/apps/webpack.config.js
+++ packages/apps/webpack.config.js
@@ -29,7 +29,7 @@ module.exports = merge(
       }),
       new AliosscdnWebpackPlugin({
         https: true,
-        directoryInOss: 'platdot/v1.0.4',
+        directoryInOss: 'platdot/v1.0.5',
         filesPath: `${__dirname}/build`,
         region: CLIENT.region,
         accessKeyId: CLIENT.accessKeyId,
