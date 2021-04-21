fb3d5cbb1914a681ad6ca2bb5b14f6ccf830fa88
吴wbh1328551759 <1328551759@qq.com>
2021-04-21 12:01:48 +0800

    update alioss dirctory name

--- packages/apps/webpack.config.js
+++ packages/apps/webpack.config.js
@@ -29,7 +29,7 @@ module.exports = merge(
       }),
       new AliosscdnWebpackPlugin({
         https: true,
-        directoryInOss: 'platdot/v1.0.5',
+        directoryInOss: 'platdot/v1.1.0',
         filesPath: `${__dirname}/build`,
         region: CLIENT.region,
         accessKeyId: CLIENT.accessKeyId,
