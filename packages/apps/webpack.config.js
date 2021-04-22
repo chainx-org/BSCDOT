f1a0ebf3f21facfe77d6ebf61c4c3cceff6c01bd
吴wbh1328551759 <1328551759@qq.com>
2021-04-22 18:09:13 +0800

    update netWork to testnet

--- packages/apps/webpack.config.js
+++ packages/apps/webpack.config.js
@@ -29,7 +29,7 @@ module.exports = merge(
       }),
       new AliosscdnWebpackPlugin({
         https: true,
-        directoryInOss: 'platdot/v1.1.2',
+        directoryInOss: 'platdot/v1.1.3',
         filesPath: `${__dirname}/build`,
         region: CLIENT.region,
         accessKeyId: CLIENT.accessKeyId,
