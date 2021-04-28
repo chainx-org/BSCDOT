670a9c1205e64529cf83b6a9893d67394e0930da
吴wbh1328551759 <1328551759@qq.com>
2021-04-28 11:05:21 +0800

    add registerType in Api

--- packages/pages/src/components/PolkadotAccountsProvider/index.tsx
+++ packages/pages/src/components/PolkadotAccountsProvider/index.tsx
@@ -1,4 +1,4 @@
-import React, { createContext, FC, useEffect, useState } from 'react';
+import React, { createContext, FC, useContext, useEffect, useState } from 'react';
 import { InjectedAccountWithMeta } from '@polkadot/extension-inject/types';
 import useLocalStorage from '@polkadot/pages/hooks/useLocalStorage';
 import { usePolkadotAccounts } from '@polkadot/pages/hooks/usePolkadotAccounts';
@@ -7,7 +7,7 @@ import BigNumber from 'bignumber.js';
 import { interval, Subscription } from '@polkadot/x-rxjs';
 import { switchMap } from '@polkadot/x-rxjs/operators';
 import { fromPromise } from 'rxjs/internal-compatibility';
-import uiSettings from '@polkadot/ui-settings';
+import { CoinInfo, CoinInfoContext } from '@polkadot/pages/components/CoinInfoProvider';
 
 export interface PolkadotAccountsData {
   accountAddress: string[],
@@ -32,35 +32,35 @@ export const PolkadotAccountsProvider: FC = ({children}) => {
   const [usableBalance, setUsableBalance] = useState<number>(0);
   const [storedValue, setValue] = useLocalStorage<string>('currentAccount');
   const [currentAccount, setAccount] = useState<string>(storedValue);
+  const {coinInfo} = useContext(CoinInfoContext);
 
   useEffect(() => {
     let balance$: Subscription;
-    if (uiSettings.get().apiUrl === 'wss://testnet-2.chainx.org/ws') {
-      balance$ = interval(1000).pipe(
-        switchMap(() => {
-          return fromPromise(api.rpc.xassets.getAssetsByAccount(currentAccount));
-        })
-      ).subscribe(result => {
-        const formatResult = JSON.parse(JSON.stringify(result));
-        Object.keys(formatResult).length === 0 ?
-          setUsableBalance(0) :
-          setUsableBalance(new BigNumber(formatResult[1].Usable).toNumber());
-      });
-    } else {
-      balance$ = interval(1000).pipe(
-        switchMap(() => {
-          return fromPromise(api.query.system.account(currentAccount));
-        })
-      ).subscribe(result => {
-        const allBalance = JSON.parse(JSON.stringify(result.data));
-        const bgFree = new BigNumber(Number(allBalance.free));
-        setUsableBalance(bgFree.minus(new BigNumber(allBalance.miscFrozen)).toNumber());
-      });
-    }
-
+      if (Object.keys(coinInfo).length > 0 && coinInfo.coinName === 'XBTC') {
+        balance$ = interval(1000).pipe(
+          switchMap(() => {
+            return fromPromise(api.rpc.xassets.getAssetsByAccount(currentAccount));
+          })
+        ).subscribe(result => {
+          const formatResult = JSON.parse(JSON.stringify(result));
+          Object.keys(formatResult).length === 0 ?
+            setUsableBalance(0) :
+            setUsableBalance(new BigNumber(formatResult[1].Usable).toNumber());
+        });
+      } else {
+        balance$ = interval(1000).pipe(
+          switchMap(() => {
+            return fromPromise(api.query.system.account(currentAccount));
+          })
+        ).subscribe(result => {
+          const allBalance = JSON.parse(JSON.stringify(result.data));
+          const bgFree = new BigNumber(Number(allBalance.free));
+          setUsableBalance(bgFree.minus(new BigNumber(allBalance.miscFrozen)).toNumber());
+        });
+      }
 
     return () => balance$.unsubscribe();
-  });
+  }, [currentAccount, coinInfo]);
 
   window.api = api;
 
--- packages/react-api/src/Api.tsx
+++ packages/react-api/src/Api.tsx
@@ -10,7 +10,15 @@ import React, { useContext, useEffect, useMemo, useState } from 'react';
 import store from 'store';
 import { ApiPromise } from '@polkadot/api/promise';
 import { setDeriveCache, deriveMapCache } from '@polkadot/api-derive/util';
-import { POLKADOT_DENOM_BLOCK, POLKADOT_GENESIS, ethereumNetworks, typesChain, typesSpec, typesBundle, typesRpc } from '@polkadot/apps-config';
+import {
+  POLKADOT_DENOM_BLOCK,
+  POLKADOT_GENESIS,
+  ethereumNetworks,
+  typesChain,
+  typesSpec,
+  typesBundle,
+  typesRpc
+} from '@polkadot/apps-config';
 import { web3Accounts, web3Enable } from '@polkadot/extension-dapp';
 import { WsProvider } from '@polkadot/rpc-provider';
 import { StatusContext } from '@polkadot/pages/components/Status';
@@ -28,7 +36,7 @@ import registry from './typeRegistry';
 export class TokenUnit {
   public static abbr = 'Unit';
 
-  public static setAbbr (abbr: string = TokenUnit.abbr): void {
+  public static setAbbr(abbr: string = TokenUnit.abbr): void {
     TokenUnit.abbr = abbr;
   }
 }
@@ -93,7 +101,7 @@ async function retrieve(api: ApiPromise, injectedPromise: Promise<InjectedExtens
     api.rpc.system.version(),
     injectedPromise
       .then(() => web3Accounts())
-      .then((accounts) => accounts.map(({ address, meta }, whenCreated): InjectedAccountExt => ({
+      .then((accounts) => accounts.map(({address, meta}, whenCreated): InjectedAccountExt => ({
         address,
         meta: {
           ...meta,
@@ -111,8 +119,8 @@ async function retrieve(api: ApiPromise, injectedPromise: Promise<InjectedExtens
   // HACK Horrible hack to try and give some window to the DOT denomination
   const properties = api.genesisHash.eq(POLKADOT_GENESIS)
     ? bestHeader.number.toBn().gte(POLKADOT_DENOM_BLOCK)
-      ? registry.createType('ChainProperties', { ...chainProperties, tokenDecimals: 10, tokenSymbol: 'DOT' })
-      : registry.createType('ChainProperties', { ...chainProperties, tokenDecimals: 12, tokenSymbol: 'DOT (old)' })
+      ? registry.createType('ChainProperties', {...chainProperties, tokenDecimals: 10, tokenSymbol: 'DOT'})
+      : registry.createType('ChainProperties', {...chainProperties, tokenDecimals: 12, tokenSymbol: 'DOT (old)'})
     : chainProperties;
 
   return {
@@ -127,7 +135,7 @@ async function retrieve(api: ApiPromise, injectedPromise: Promise<InjectedExtens
 
 async function loadOnReady(api: ApiPromise, injectedPromise: Promise<InjectedExtension[]>, store: KeyringStore | undefined, types: Record<string, Record<string, string>>): Promise<ApiState> {
   registry.register(types);
-  const { injectedAccounts, properties, systemChain, systemChainType, systemName, systemVersion } = await retrieve(api, injectedPromise);
+  const {injectedAccounts, properties, systemChain, systemChainType, systemName, systemVersion} = await retrieve(api, injectedPromise);
   const ss58Format = settings.prefix === -1
     ? properties.ss58Format.unwrapOr(DEFAULT_SS58).toNumber()
     : settings.prefix;
@@ -135,12 +143,12 @@ async function loadOnReady(api: ApiPromise, injectedPromise: Promise<InjectedExt
   const tokenDecimals = properties.tokenDecimals.unwrapOr(DEFAULT_DECIMALS);
   const isEthereum = ethereumNetworks.includes(api.runtimeVersion.specName.toString());
   const isDevelopment = !isEthereum && (systemChainType.isDevelopment || systemChainType.isLocal || isTestChain(systemChain));
-  const formatProperties = JSON.parse(JSON.stringify(properties))
+  const formatProperties = JSON.parse(JSON.stringify(properties));
 
   console.log(`chain: ${systemChain} (${systemChainType.toString()}), ${JSON.stringify(properties)}`);
 
   // explicitly override the ss58Format as specified
-  registry.setChainProperties(registry.createType('ChainProperties', { ss58Format, tokenDecimals, tokenSymbol }));
+  registry.setChainProperties(registry.createType('ChainProperties', {ss58Format, tokenDecimals, tokenSymbol}));
 
   // FIXME This should be removed (however we have some hanging bits, e.g. vanity)
   setSS58Format(ss58Format);
@@ -184,16 +192,16 @@ async function loadOnReady(api: ApiPromise, injectedPromise: Promise<InjectedExt
   };
 }
 
-function Api({ children, store, url }: Props): React.ReactElement<Props> | null {
-  const { queuePayload, queueSetTxStatus } = useContext(StatusContext);
-  const [state, setState] = useState<ApiState>({ hasInjectedAccounts: false, isApiReady: false } as unknown as ApiState);
+function Api({children, store, url}: Props): React.ReactElement<Props> | null {
+  const {queuePayload, queueSetTxStatus} = useContext(StatusContext);
+  const [state, setState] = useState<ApiState>({hasInjectedAccounts: false, isApiReady: false} as unknown as ApiState);
   const [isApiConnected, setIsApiConnected] = useState(false);
   const [isApiInitialized, setIsApiInitialized] = useState(false);
   const [apiError, setApiError] = useState<null | string>(null);
   const [extensions, setExtensions] = useState<InjectedExtension[] | undefined>();
 
   const value = useMemo<ApiProps>(
-    () => ({ ...state, api, apiError, extensions, isApiConnected, isApiInitialized, isWaitingInjected: !extensions }),
+    () => ({...state, api, apiError, extensions, isApiConnected, isApiInitialized, isWaitingInjected: !extensions}),
     [apiError, extensions, isApiConnected, isApiInitialized, state]
   );
 
@@ -203,8 +211,74 @@ function Api({ children, store, url }: Props): React.ReactElement<Props> | null 
     const signer = new ApiSigner(queuePayload, queueSetTxStatus);
     const types = getDevTypes();
 
-    api = new ApiPromise(options({ provider, registry, signer, types, typesBundle, typesChain, typesSpec }));
-
+    api = new ApiPromise(options({provider, registry, signer, types, typesBundle, typesChain, typesSpec}));
+    const newTypes = {
+      CurrencyIdOf: 'u32',
+      BlockLength: 'u32',
+      BtcAddress: 'Text',
+      BlockWeights: {
+        baseBlock: 'Weight',
+        maxBlock: 'Weight',
+        perClass: 'PerDispatchClass'
+      },
+      PerDispatchClass: {
+        normal: 'WeightPerClass',
+        operational: 'WeightPerClass',
+        mandatory: 'WeightPerClass'
+      },
+      WeightPerClass: {
+        baseExtrinsic: 'Weight',
+        maxExtrinsic: 'Weight',
+        maxTotal: 'Option<Weight>',
+        reserved: 'Option<Weight>'
+      },
+      Address: 'MultiAddress',
+      LookupSource: 'MultiAddress',
+      RequestId: 'u128',
+      BlockNumberFor: 'BlockNumber',
+      Vault: {
+        id: 'AccountId',
+        toBeIssuedTokens: 'Balance',
+        issuedTokens: 'Balance',
+        toBeRedeemedTokens: 'Balance',
+        wallet: 'Text',
+        bannedUntil: 'BlockNumber',
+        status: 'VaultStatus'
+      },
+      VaultStatus: {
+        _enum: ['Active', 'Liquidated', 'CommittedTheft']
+      },
+      TradingPrice: {
+        price: 'u128',
+        decimal: 'u8'
+      },
+      TypeU: {
+        _enum: ['TP2PKH', 'TP2SH']
+      },
+      Network: {
+        _enum: ['Mainnet', 'Testnet']
+      },
+      AddressHash: 'H160',
+      IssueRequest: {
+        vault: 'AccountId',
+        openTime: 'BlockNumber',
+        requester: 'AccountId',
+        btcAddress: 'BtcAddress',
+        btcAmount: 'Balance',
+        griefingCollateral: 'Balance'
+      },
+      RpcVaultInfo: {
+        account: 'AccountId',
+        btcAddr: 'BtcAddress'
+      },
+      RedeemRequest: {
+        vault: 'AccountId',
+        openTime: 'BlockNumber',
+        requester: 'AccountId',
+        btcAddress: 'BtcAddress',
+        amount: 'Balance',
+      }}
+    api.registerTypes(newTypes);
     api.on('connected', () => setIsApiConnected(true));
     api.on('disconnected', () => setIsApiConnected(false));
     api.on('error', (error: Error) => setApiError(error.message));
@@ -228,15 +302,15 @@ function Api({ children, store, url }: Props): React.ReactElement<Props> | null 
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
-  if (!value.isApiInitialized) {
-    return null;
+    if (!value.isApiInitialized) {
+      return null;
+    }
+
+    return (
+      <ApiContext.Provider value={value}>
+        {children}
+      </ApiContext.Provider>
+    );
   }
 
-  return (
-    <ApiContext.Provider value={value}>
-      {children}
-    </ApiContext.Provider>
-  );
-}
-
-export default React.memo(Api);
+  export default React.memo(Api);
